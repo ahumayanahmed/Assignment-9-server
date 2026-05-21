f2d45d82e470382dc145a0f7cf6b2a3394ecb287
ahumayanahmed <ahumayan962@gmail.com>
2026-05-21 14:52:34 +0600

    final

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const { createRemoteJWKSet , jwtVerify} = require("jose-cjs");
+
+const {
+  MongoClient,
+  ServerApiVersion,
+  ObjectId,
+} = require("mongodb");
+
+const {
+  createRemoteJWKSet,
+  jwtVerify,
+} = require("jose-cjs");
 
 dotenv.config();
 
 const app = express();
+
 const PORT = process.env.PORT || 8000;
 
 app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://assignment-9-client-eosin.vercel.app",
+       "https://assignment-9-client-eosin.vercel.app",
     ],
     credentials: true,
   })
 );
+
 app.use(express.json());
 
 const uri = process.env.MONGODB_URL;
@@ -30,237 +41,299 @@ const client = new MongoClient(uri, {
   },
 });
 
+const JWKS = createRemoteJWKSet(
+  new URL(`${process.env.BETTER_AUTH_URL}/api/auth/jwks`)
+);
 
- const JWKS = createRemoteJWKSet(new URL(`${process.env.BETTER_AUTH_URL}/api/auth/jwks`));
 
+
+// VERIFY TOKEN
 const verifyToken = async (req, res, next) => {
-  const authHeader = req?.headers.authorization;
+
+  const authHeader = req.headers.authorization;
 
   if (!authHeader) {
-    return res.status(401).json({ message: "Unauthorized" });
+    return res.status(401).send({
+      message: "Unauthorized Access",
+    });
   }
 
   const token = authHeader.split(" ")[1];
 
   if (!token) {
-    return res.status(401).json({ message: "Unauthorized" });
+    return res.status(401).send({
+      message: "Unauthorized Access",
+    });
   }
 
   try {
 
     const { payload } = await jwtVerify(token, JWKS);
 
-    // email save
     req.user = payload;
-
-    // email match check
-    if (
-      req.params.email &&
-      payload.email !== req.params.email
-    ) {
-      return res
-        .status(403)
-        .send({ message: "Forbidden Access" });
-    }
 
     next();
 
   } catch (error) {
-    return res.status(403).json({ message: "Forbidden" });
+
+    return res.status(403).send({
+      message: "Forbidden Access",
+    });
+
   }
 };
 
 
+
+
 async function run() {
+
   try {
-    // await client.connect();
 
     const db = client.db("assignment9");
 
     const bookingCollection = db.collection("booking");
+
     const bookingsCollection = db.collection("bookings");
 
-   app.post("/booking",verifyToken,  async (req, res) => {
+
+
+    
+
+    app.post("/booking", verifyToken, async (req, res) => {
+
       const bookingData = req.body;
-      console.log(bookingData);
-      const result = await bookingCollection.insertOne(bookingData);
-      res.json(result);
+
+      const result = await bookingCollection.insertOne(
+        bookingData
+      );
+
+      res.send(result);
+
     });
 
-app.get("/booking/:email", async (req, res) => {
 
-  const email = req.params.email;
 
-  const query = {
-    ownerEmail: email,
-  };
+   
 
-  const result = await bookingCollection
-    .find(query)
-    .toArray();
+    app.get("/booking", async (req, res) => {
 
-  res.send(result);
-});
+      try {
 
-    // app.get("/booking", async (req, res) => {
-    //   const result = await bookingCollection.find().toArray();
-    //   res.json(result);
-    // });
+        const {
+          search,
+          amenity,
+          floor,
+          price,
+          sort,
+        } = req.query;
 
-    app.patch("/booking/:id",verifyToken, async (req, res) => {
-  try {
-    const id = req.params.id;
-    const updatedRoom = req.body;
+        let query = {};
 
-    const result = await bookingCollection.updateOne(
-      { _id: new ObjectId(id) },
-      {
-        $set: {
-          name: updatedRoom.name,
-          image: updatedRoom.image,
-          floor: updatedRoom.floor,
-          capacity: updatedRoom.capacity,
-          hourlyRate: updatedRoom.hourlyRate,
-          description: updatedRoom.description,
-          amenities: updatedRoom.amenities,
-        },
+        if (search) {
+          query.name = {
+            $regex: search,
+            $options: "i",
+          };
+        }
+
+        if (amenity) {
+          query.amenities = {
+            $in: [amenity],
+          };
+        }
+
+        if (floor) {
+          query.floor = floor;
+        }
+
+        let sortOption = {};
+
+        if (sort === "newest") {
+          sortOption = { _id: -1 };
+        }
+
+        if (sort === "oldest") {
+          sortOption = { _id: 1 };
+        }
+
+        if (price === "low") {
+          sortOption = { hourlyRate: 1 };
+        }
+
+        if (price === "high") {
+          sortOption = { hourlyRate: -1 };
+        }
+
+        const rooms = await bookingCollection
+          .find(query)
+          .sort(sortOption)
+          .toArray();
+
+        res.send(rooms);
+
+      } catch (error) {
+
+        res.status(500).send({
+          error: error.message,
+        });
+
       }
-    );
 
-    res.send({
-      success: true,
-      message: "Room updated successfully",
-      result,
     });
 
-  } catch (error) {
-    res.status(500).send({
-      success: false,
-      error: error.message,
-    });
-  }
-});
 
 
+    
 
-app.get("/booking", async (req, res) => {
+ app.get("/booking/my/:email", verifyToken, async (req, res) => {
   try {
-    const { search, amenity, floor, price, sort } = req.query;
+    const email = req.params.email;
 
-    let query = {};
+    console.log("URL EMAIL:", email);
+    console.log("TOKEN EMAIL:", req.user.email);
 
-    // SEARCH
-    if (search) {
-      query.name = {
-        $regex: search,
-        $options: "i",
-      };
-    }
-
-    // AMENITY
-    if (amenity) {
-      query.amenities = {
-        $in: [amenity],
-      };
-    }
-
-    // FLOOR
-    if (floor) {
-      query.floor = floor;
-    }
-
-    // SORT
-    let sortOption = {};
-
-    if (sort === "newest") {
-      sortOption = { _id: -1 };
-    }
-
-    if (sort === "oldest") {
-      sortOption = { _id: 1 };
-    }
-
-    if (price === "low") {
-      sortOption = { hourlyRate: 1 };
-    }
-
-    if (price === "high") {
-      sortOption = { hourlyRate: -1 };
-    }
-
-    const rooms = await bookingCollection
-      .find(query)
-      .sort(sortOption)
+    const result = await bookingCollection
+      .find({ ownerEmail: email })
       .toArray();
 
-    res.send(rooms);
+    console.log("RESULT:", result);
 
-  } catch (error) {
-    res.status(500).send({
-      error: error.message,
-    });
-  }
-});
-
-
-
-
-app.post("/bookings",verifyToken, async (req, res) => {
-  try {
-    const booking = req.body;
-
-    if (!booking.date || !booking.time) {
-      return res.status(400).send({ message: "Invalid data" });
-    }
-
-    const result = await bookingsCollection.insertOne(booking);
-
-    res.send({
-      success: true,
-      message: "Booking saved successfully",
-      insertedId: result.insertedId,
-    });
+    res.send(result);
   } catch (err) {
-    res.status(500).send({ success: false, error: err.message });
+    res.status(500).send({ error: err.message });
   }
 });
-app.get("/bookings", async (req, res) => {
-      const result = await bookingsCollection.find().toArray();
-      res.json(result);
+
+
+    
+
+    app.patch("/booking/:id", verifyToken, async (req, res) => {
+
+      try {
+
+        const id = req.params.id;
+
+        const updatedRoom = req.body;
+
+        const result = await bookingCollection.updateOne(
+          {
+            _id: new ObjectId(id),
+            ownerEmail: req.user.email,
+          },
+          {
+            $set: {
+              name: updatedRoom.name,
+              image: updatedRoom.image,
+              floor: updatedRoom.floor,
+              capacity: updatedRoom.capacity,
+              hourlyRate: updatedRoom.hourlyRate,
+              description: updatedRoom.description,
+              amenities: updatedRoom.amenities,
+            },
+          }
+        );
+
+        res.send({
+          success: true,
+          message: "Room updated successfully",
+          result,
+        });
+
+      } catch (error) {
+
+        res.status(500).send({
+          success: false,
+          error: error.message,
+        });
+
+      }
+
     });
 
 
-const { ObjectId } = require("mongodb");
 
-app.delete("/booking/:id",verifyToken, async (req, res) => {
+    
+
+    app.delete("/booking/:id", verifyToken, async (req, res) => {
+
+      try {
+
+        const id = req.params.id;
+
+        const result = await bookingCollection.deleteOne({
+          _id: new ObjectId(id),
+          ownerEmail: req.user.email,
+        });
+
+        res.send({
+          success: true,
+          message: "Room deleted successfully",
+          result,
+        });
+
+      } catch (error) {
+
+        res.status(500).send({
+          success: false,
+          error: error.message,
+        });
+
+      }
+
+    });
+
+
+
+  
+
+    app.post("/bookings", verifyToken, async (req, res) => {
+  const booking = req.body;
+
+  const newBooking = {
+    ...booking,
+    ownerEmail: req.user.email, 
+  };
+
+  const result = await bookingsCollection.insertOne(newBooking);
+
+  res.send({
+    success: true,
+    insertedId: result.insertedId,
+  });
+});
+
+
+
+ 
+
+ app.get("/bookings", verifyToken, async (req, res) => {
   try {
-    const id = req.params.id;
+    const email = req.user.email;
 
-    const result = await bookingCollection.deleteOne({
-      _id: new ObjectId(id),
-    });
+    const result = await bookingsCollection
+      .find({ ownerEmail: email }) // IMPORTANT
+      .toArray();
 
-    res.send({
-      success: true,
-      message: "Booking deleted",
-      result,
-    });
+    res.send(result);
   } catch (err) {
-    res.status(500).send({
-      success: false,
-      error: err.message,
-    });
+    res.status(500).send({ error: err.message });
   }
 });
 
 
 
-app.patch("/bookings/:id",verifyToken, async (req, res) => {
+    
+
+    app.patch("/bookings/:id", verifyToken, async (req, res) => {
   try {
     const id = req.params.id;
 
     const result = await bookingsCollection.updateOne(
-      { _id: new ObjectId(id) },
+      {
+        _id: new ObjectId(id),
+        ownerEmail: req.user.email, // FIXED
+      },
       {
         $set: {
           status: "cancelled",
@@ -273,24 +346,27 @@ app.patch("/bookings/:id",verifyToken, async (req, res) => {
       message: "Booking cancelled",
       result,
     });
-  } catch (err) {
+
+  } catch (error) {
     res.status(500).send({
       success: false,
-      error: err.message,
+      error: error.message,
     });
   }
 });
 
 
-    // await client.db("admin").command({ ping: 1 });
 
     console.log("MongoDB Connected");
+
   } finally {
+
   }
+
 }
 
 run().catch(console.dir);
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
